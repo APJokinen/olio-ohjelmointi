6335ea558a849b6a7e9d291b7016c2696a705959
Ari-Pekka Jokinen <t3joar00@students.oamk.fi>
2024-01-15 15:06:07 +0200

    functionExample2 valmis

--- Viikko2/functionExample2/main.cpp
+++ Viikko2/functionExample2/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+//(1)kirjoita funktio, joka ottaa vastaan kaksi kokonaislukua
+//funktio kertoo molemmat luvut kahdella ja tulostaa summan
+//tulosta myös alkuperäiset luvut
+
+//(2) kirjoita funktio kuten edellä, mutta funktion on tuplattava
+// alkuperäisten muuttujien arvot
+//nyt funktiossa on otettava vastaan muuttujien osoittimet
+//funktion kutsussa argumentteina annetaan osoitteet (&x ja &y)
 
 using namespace std;
 
+void doubleNumbers1(int x, int y);
+void doubleNumbers2(int *x, int *y);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int x=5, y=9;
+    cout << "Aluksi mainissa x=" << x << endl;
+    cout << "Aluksi mainissa y=" << y << endl;
+    doubleNumbers1(x,y);
+    cout << "Lopuksi mainissa x=" << x << endl;
+    cout << "Lopuksi mainissa y=" << y << endl;
+
+    doubleNumbers2(&x, &y);
+    cout << "Lopuksi mainissa x=" << x << endl;
+    cout << "Lopuksi mainissa y=" << y << endl;
     return 0;
+}
+
+void doubleNumbers1(int x, int y){
+    x=x * 2;
+    y=y * 2;
+    cout << "funktiossa(1) x=" << x << endl;
+    cout << "funktiossa(1) y=" << y << endl;
+}
+
+void doubleNumbers2(int *x, int *y){
+    *x=*x * 2;
+    *y=*y * 2;
+    cout << "funktiossa(2) x=" << *x << endl;
+    cout << "funktiossa(2) y=" << *y << endl;
 }
